35bf73c30148fa8b877c174326bf1f687ea5ba2d
li-lauren <laurenlidev@gmail.com>
2020-11-03 03:29:18 +0000

    Add Login and SignUp forms

--- static/js/App.jsx
+++ static/js/App.jsx
@@ -1,9 +1,78 @@
+const Login = () => {
+    const [userInput, setUserInput] = React.useReducer(
+        (state, newState) => ({...state, ...newState}), 
+        {
+            email: '', 
+            pw: ''
+        }
+    );
 
+    const handleChange = e => {
+        const name = e.target.name;
+        const value = e.target.value;
 
-const App = () => {
+        setUserInput({[name]: value});
+    }
+
+    // add onSubmit
+
     return(
         <div>
+            <label>Email</label>
+            <input type="text" name="email" value={userInput.email} onChange={handleChange} />
+            <label>Password</label>
+            <input type="text" name="pw" value={userInput.pw} onChange={handleChange} />
+            <button type="submit">Login</button>
+        </div>   
+    )
+}
 
+const SignUp = () => {
+    const [userInput, setUserInput] = React.useReducer(
+        (state, newState) => ({...state, ...newState}), 
+        {
+            fname: '', 
+            lname: '',
+            email: '', 
+            pw: '', 
+            img: ''
+        }
+    );
+
+    const handleChange = e => {
+        const name = e.target.name;
+        const value = e.target.value;
+
+        setUserInput({[name]: value});
+    }
+
+    // add onSubmit
+
+    return(
+        <div>
+            <br/>
+            <label>First Name</label>
+            <input type="text" name="fname" value={userInput.fname} onChange={handleChange} />
+            <label>Last Name</label>
+            <input type="text" name="lname" value={userInput.lname} onChange={handleChange} />
+            
+            <br/>
+            <label>Email</label>
+            <input type="text" name="email" value={userInput.email} onChange={handleChange} />
+            <label>Password</label>
+            <input type="text" name="pw" value={userInput.pw} onChange={handleChange} />
+            <br/>
+            <button type="submit">Join Jot</button>
+        </div>   
+    )
+}
+
+const App = () => {
+
+    return(
+        <div>
+            <Login />
+            <SignUp />
         </div>
     )
 }
