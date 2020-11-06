a45d38e56592d8eda77e448076b6a66841c83ad4
li-lauren <laurenlidev@gmail.com>
2020-11-06 20:01:13 +0000

    Move Login and Signup into individual jsx files

--- static/js/App.jsx
+++ static/js/App.jsx
@@ -35,13 +35,6 @@ const postNote = (room, note) => {
     }
 }
 
-const loadInitialNotes = (cb) => {
-    if (!socket) {
-        return true;
-    }
-    console.log('Loading initial notes');
-    socket.on('join_msg', note => cb(null, note));
-}
 
 // End Socket functions
 
@@ -345,144 +338,8 @@ const AddDoc = ({docAdded, setDocAdded}) => {
 
 
 
-const Login = () => {
-    const [userInput, setUserInput] = React.useReducer(
-        (state, newState) => ({...state, ...newState}), 
-        {
-            email: '', 
-            pw: ''
-        }
-    );
-    const [errorMsg, setErrorMsg] = React.useState('')
-    const [loggedIn, setLoggedIn] = React.useState(false)
-    // const [docs, setDocs] = React.useState([])
 
-    const handleChange = e => {
-        const name = e.target.name;
-        const value = e.target.value;
 
-        setUserInput({[name]: value});
-    }
-
-    const login = (e) => {
-        e.preventDefault()
-        console.log('LOGIN')
-        console.log(`UserInput: ${userInput}`)
-
-        const reqOptions = {
-            method: 'POST',
-            headers: {
-                'Content-Type':'application/json'
-              },
-            body: JSON.stringify({
-                'email': userInput.email, 
-                'pw': userInput.pw })
-        }
-
-        fetch("/login", reqOptions)
-        .then(res => res.text())
-        .then(data => {
-            setErrorMsg(data)
-            setLoggedIn(true)
-            // if (typeof(data) === 'string') {
-            //     setErrorMsg(data)
-            // } else {
-            //     setDocs(data)
-            //     for (const doc of docs) {
-            //         console.log(`Success: ${doc.title}`)
-            //     }  
-            //     setErrorMsg('')
-            // }   
-        })
-    }
-
-    return(
-        <div>
-            <h5>Login</h5>
-            <form onSubmit={login} >
-                <label>Email</label>
-                <input type="text" name="email" value={userInput.email} onChange={handleChange} />
-                <label>Password</label>
-                <input type="password" name="pw" value={userInput.pw} onChange={handleChange} />
-                <br/>
-                <button type="submit">Login</button>
-            </form>
-            <p>{errorMsg}</p>  
-            <Dashboard loggedIn={loggedIn} />
-            {/* <AddDoc setDocs={setDocs} />
-            <DocList docs={docs}/>   */}
-            
-        </div>   
-    )
-}
-
-const SignUp = () => {
-    const [userInput, setUserInput] = React.useReducer(
-        (state, newState) => ({...state, ...newState}), 
-        {
-            fname: '', 
-            lname: '',
-            email: '', 
-            pw: '', 
-            img: ''
-        }
-    );
-    const [msg, setMsg] = React.useState('')
-
-    const handleChange = e => {
-        const name = e.target.name;
-        const value = e.target.value;
-
-        setUserInput({[name]: value});
-    }
-
-    const signUp = (e) => {
-        e.preventDefault()
-        console.log('SIGNUP')
-        console.log(`UserInput: ${userInput}`)
-
-        const reqOptions = {
-            method: 'POST',
-            headers: {
-                'Content-Type':'application/json'
-              },
-            body: JSON.stringify({
-                'fname': userInput.fname,
-                'lname': userInput.lname,
-                'email': userInput.email, 
-                'pw': userInput.pw, 
-                'img': userInput.img 
-            })
-        }
-        fetch("/users", reqOptions)
-        .then(res => res.text())
-        .then(data => {
-            console.log(data)
-            setMsg(data)
-        })
-    }
-
-    return(
-        <div>
-            <h5>Sign Up</h5>
-            <form onSubmit={signUp}>
-                <label>First Name</label>
-                <input type="text" name="fname" value={userInput.fname} onChange={handleChange} />
-                <label>Last Name</label>
-                <input type="text" name="lname" value={userInput.lname} onChange={handleChange} />
-                
-                <br/>
-                <label>Email</label>
-                <input type="text" name="email" value={userInput.email} onChange={handleChange} />
-                <label>Password</label>
-                <input type="password" name="pw" value={userInput.pw} onChange={handleChange} />
-                <br/>
-                <button type="submit">Join Jot</button>
-            </form> 
-            <p>{msg}</p>  
-        </div>   
-    )
-}
 
 
 
