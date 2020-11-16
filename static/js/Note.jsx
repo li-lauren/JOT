04320b315279c9afb69b8df60d9fd9980aa82cc7
li-lauren <laurenlidev@gmail.com>
2020-11-16 01:33:23 +0000

    Clean up Note.jsx

--- static/js/Note.jsx
+++ static/js/Note.jsx
@@ -11,80 +11,18 @@ const getPos = (cb) => {
     })
 }
 
-
-// const Note = ({note, room}) => {
-//     const { width, height } = updateWindowDim()
-//     const [pos, setPos] = React.useState(
-//         { x: note.x_pos, y: note.y_pos * height })
-//     const note_id = note.note_id
-    
-//     React.useEffect(() => {
-//         getPos((error, data) => {
-//             if (error) {
-//                 return "Error getting note position"
-//             }
-//             console.log(`Data: ${data.x} ${data.y}`)
-            
-//             if (note_id == data.note_id) {
-//                 console.log(`Matching note ids: ${note_id} and ${data.note_id} `)
-//                 const normX = data.x
-//                 const normY = data.y * height
-//                 console.log({ normX, normY })
-//                 setPos({ x: normX, y: normY })
-//             }
-            
-//         });
-//     })
-
-//     const trackPos = (data) => {
-//         setPos({x: data.x, y: data.y})
-//         // socket.emit('receive_position', { 'x': data.x, 'y': data.y })
-//     }
-
-//     const updatePos = (data) => {
-//         setPos({x: data.x, y: data.y})
-//         console.log(`NOTE ID ${note_id}`)
-//         socket.emit('fin_pos', { 
-//             'x': data.x, 
-//             'y': data.y / height, 
-//             'note_id': note_id, 
-//             'room': room })
-//     }
-
-//     return(
-//         <ReactDraggable
-//             // defaultPosition={{x: pos.x, y: pos.y}}
-//             axis="none"
-//             position={null}
-//             onDrag={(e, data) => trackPos(data)}
-//             onStop={(e, data) => updatePos(data)}
-//         >
-//             <Button id="note" onClick={()=>console.log(note_id)}>
-//                 <span>{note.note_id}:</span>
-//                 <br/>
-//                 {note.body}
-//                 <br/>
-//                 {/* {`x: ${pos.x.toFixed(0)}, y: ${pos.y.toFixed(0)}`} */}
-//                 {`x: ${(pos.x).toFixed(2)}, y: ${(pos.y / height).toFixed(2)}`}
-//             </Button>
-
-//         </ReactDraggable>
-        
-//     )
-// }
-
 const Note = ({note, room}) => {
     // const { width, height } = updateWindowDim()
-    const [pos, setPos] = React.useState(
+    const [pos, setPos] = useState(
         { x: note.x_pos, y: note.y_pos })
-    const [isHovering, setIsHovering] = React.useState(false)
+    const [isHovering, setIsHovering] = useState(false)
 
     const note_id = note.note_id
     const fname = note.fname
     const lname = note.lname
     const created_at = new Date(note.created_at)
     
-    React.useEffect(() => {
+    useEffect(() => {
         getPos((error, data) => {
             if (error) {
                 return "Error getting note position"
