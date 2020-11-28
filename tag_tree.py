8a65a3a008f2428a71dda87f17a744feec67d6a2
li-lauren <laurenlidev@gmail.com>
2020-11-28 23:54:32 +0000

    Integrate D3 with hardcoded tag data

---
+++ tag_tree.py
@@ -0,0 +1,154 @@
+tag_tree = {
+    "name": "All",
+    "children": [
+        {
+            "name": "STEM", 
+            "children": [
+                {
+                    "name": "Science",
+                    "children": [
+                        {
+                            "name": "Astronomy", 
+                            "children": [
+
+                            ]
+                        },
+                        {
+                            "name": "History", 
+                            "children": [
+                                {
+                                    "name": "Hundreds of Copies of Newton's Principia Found in New Census", 
+                                    "doc_id": 9, 
+                                    "value": 25
+                                }
+                            ]
+                        },
+                        {
+                            "name": "Math",
+                            "children": [
+                                {
+                                    "name": "Erik Demaine wins 2020 MIT Bose Award for Excellence in Teaching", 
+                                    "doc_id": 16,
+                                    "value": 10
+                                }
+                            ]
+                        }, 
+                        {
+                            "name": "Biology",
+                            "children": [
+                                {
+                                    "name": "The Last Children of Down Syndrome",
+                                    "doc_id": 6,
+                                    "value": 14
+                                },
+                                {
+                                    "name": "Neuroscientist Viviana Gradinaru Receives Young Investigator Award",
+                                    "doc_id": 10,
+                                    "value": 8
+                                },
+                                {
+                                    "name": "How Stem Cells Choose their Careers",
+                                    "doc_id": 11,
+                                    "value": 21
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    "name": "Technology",
+                    "children": [
+                        {
+                            "name": "Computer Science",
+                            "children": [
+                                {
+                                    "name": "A neural network learns when it should not be trusted",
+                                    "doc_id": 15,
+                                    "value": 18
+                                }
+                            ]
+                        },
+                        {
+                            "name": "Engineering", 
+                            "children": [
+                                {
+                                    "name": "How the F@!# Did This Giant Whale Tail Save a Derailed Train?", 
+                                    "doc_id": 3, 
+                                    "value": 12
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            "name": "Arts/Entertainment", 
+            "children": [
+                {
+                    "name": "Arts", 
+                    "children": [
+                        {
+                            "name": "Design", 
+                            "children": [
+                                {
+                                    "name": "Editorial Design : New Brutalism Controversial Concrete", 
+                                    "doc_id": 4,
+                                    "value": 24
+                                }
+                            ]
+                        },
+                        {
+                            "name": "Fine Arts", 
+                            "children": [
+                                {
+                                    "name": "Prized Hockney Portrait Will Stay at London’s Royal Opera House Following Sale: Report", 
+                                    "doc_id": 5, 
+                                    "value": 14
+                                }
+
+                            ]
+                        },
+                        {
+                            "name": "Architecture", 
+                            "children": [
+                                {
+                                    "name": "Public opinion has softened its views on Brutalism. That isn’t enough to stay the wrecking ball.",
+                                    "doc_id": 1,
+                                    "value": 35
+                                }, 
+                                {
+                                    "name": "Editorial Design : New Brutalism Controversial Concrete", 
+                                    "doc_id": 4,
+                                    "value": 24
+                                }
+                            ]
+                        }
+                    ]
+                }, 
+                {
+                    "name": "Entertainment",
+                    "children": [
+                        {
+                            "name": "Books"
+                        },
+                        {
+                            "name": "Movies",
+                            "children": [
+                                {
+                                    "name": "Netflix’s Rebecca flattens a classic", 
+                                    "doc_id": 12, 
+                                    "value": 23
+                                }
+
+                            ]
+                        }
+                    ]
+                }
+            ]
+        }, 
+        {
+            "name": "Other"
+        }
+    ]
+}
