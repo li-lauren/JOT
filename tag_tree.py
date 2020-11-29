271f9fb86de370f0b36b75787c2a1e1818849033
li-lauren <laurenlidev@gmail.com>
2020-11-29 17:23:01 +0000

    Add get_leaves

--- tag_tree.py
+++ tag_tree.py
@@ -1,4 +1,4 @@
-tag_tree = {
+tag_tree = [{
     "name": "All",
     "children": [
         {
@@ -16,41 +16,19 @@ tag_tree = {
                         {
                             "name": "History", 
                             "children": [
-                                {
-                                    "name": "Hundreds of Copies of Newton's Principia Found in New Census", 
-                                    "doc_id": 9, 
-                                    "value": 25
-                                }
+                                
                             ]
                         },
                         {
                             "name": "Math",
                             "children": [
-                                {
-                                    "name": "Erik Demaine wins 2020 MIT Bose Award for Excellence in Teaching", 
-                                    "doc_id": 16,
-                                    "value": 10
-                                }
+                                
                             ]
                         }, 
                         {
                             "name": "Biology",
                             "children": [
-                                {
-                                    "name": "The Last Children of Down Syndrome",
-                                    "doc_id": 6,
-                                    "value": 14
-                                },
-                                {
-                                    "name": "Neuroscientist Viviana Gradinaru Receives Young Investigator Award",
-                                    "doc_id": 10,
-                                    "value": 8
-                                },
-                                {
-                                    "name": "How Stem Cells Choose their Careers",
-                                    "doc_id": 11,
-                                    "value": 21
-                                }
+                            
                             ]
                         }
                     ]
@@ -61,21 +39,13 @@ tag_tree = {
                         {
                             "name": "Computer Science",
                             "children": [
-                                {
-                                    "name": "A neural network learns when it should not be trusted",
-                                    "doc_id": 15,
-                                    "value": 18
-                                }
+                                
                             ]
                         },
                         {
                             "name": "Engineering", 
                             "children": [
-                                {
-                                    "name": "How the F@!# Did This Giant Whale Tail Save a Derailed Train?", 
-                                    "doc_id": 3, 
-                                    "value": 12
-                                }
+                                
                             ]
                         }
                     ]
@@ -114,37 +84,19 @@ tag_tree = {
                         {
                             "name": "Design", 
                             "children": [
-                                {
-                                    "name": "Editorial Design : New Brutalism Controversial Concrete", 
-                                    "doc_id": 4,
-                                    "value": 24
-                                }
+                                
                             ]
                         },
                         {
                             "name": "Fine Arts", 
                             "children": [
-                                {
-                                    "name": "Prized Hockney Portrait Will Stay at London’s Royal Opera House Following Sale: Report", 
-                                    "doc_id": 5, 
-                                    "value": 14
-                                }
-
+                                
                             ]
                         },
                         {
                             "name": "Architecture", 
                             "children": [
-                                {
-                                    "name": "Public opinion has softened its views on Brutalism. That isn’t enough to stay the wrecking ball.",
-                                    "doc_id": 1,
-                                    "value": 35
-                                }, 
-                                {
-                                    "name": "Editorial Design : New Brutalism Controversial Concrete", 
-                                    "doc_id": 4,
-                                    "value": 24
-                                }
+                                
                             ]
                         }
                     ]
@@ -240,4 +192,25 @@ tag_tree = {
             ]
         }
     ]
-}
+}]
+
+
+
+def get_leaves(tag_tree):
+    if not tag_tree:
+        return []
+
+    leaf_tag = []
+    
+    for obj in tag_tree:
+        if not obj.get("children"):
+            leaf_tag.extend([obj.get("name")])
+        else:
+            leaf_tag.extend(get_leaves(obj.get("children")))
+    
+    return leaf_tag 
+
+
+print(get_leaves(tag_tree))
+
+    
