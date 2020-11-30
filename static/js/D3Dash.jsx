be434945e341c26161ebc9f89f876148bc9acf87
li-lauren <laurenlidev@gmail.com>
2020-11-30 05:07:45 +0000

    Incorporate photos in tag tree

--- static/js/D3Dash.jsx
+++ static/js/D3Dash.jsx
@@ -47,249 +47,7 @@ const D3Dash = () => {
             .size([diameter - margin, diameter - margin])
             .padding(2);
 
-        // let root = {
-        //     "name": "All",
-        //     "children": [
-        //         {
-        //             "name": "Science", 
-        //             "children": [
-        //                 {
-        //                     "name": "Science",
-        //                     "children": [
-        //                         {
-        //                             "name": "Astronomy", 
-        //                             "children": [
         
-        //                             ]
-        //                         },
-        //                         {
-        //                             "name": "History", 
-        //                             "children": [
-        //                                 {
-        //                                     "name": "Hundreds of Copies of Newton's Principia Found in New Census", 
-        //                                     "doc_id": 9, 
-        //                                     "value": 25
-        //                                 }
-        //                             ]
-        //                         },
-        //                         {
-        //                             "name": "Math",
-        //                             "children": [
-        //                                 {
-        //                                     "name": "Erik Demaine wins 2020 MIT Bose Award for Excellence in Teaching", 
-        //                                     "doc_id": 16,
-        //                                     "value": 10
-        //                                 }
-        //                             ]
-        //                         }, 
-        //                         {
-        //                             "name": "Biology",
-        //                             "children": [
-        //                                 {
-        //                                     "name": "The Last Children of Down Syndrome",
-        //                                     "doc_id": 6,
-        //                                     "value": 14
-        //                                 },
-        //                                 {
-        //                                     "name": "Neuroscientist Viviana Gradinaru Receives Young Investigator Award",
-        //                                     "doc_id": 10,
-        //                                     "value": 8
-        //                                 },
-        //                                 {
-        //                                     "name": "How Stem Cells Choose their Careers",
-        //                                     "doc_id": 11,
-        //                                     "value": 21
-        //                                 }
-        //                             ]
-        //                         }
-        //                     ]
-        //                 },
-        //                 {
-        //                     "name": "Technology",
-        //                     "children": [
-        //                         {
-        //                             "name": "Computer Science",
-        //                             "children": [
-        //                                 {
-        //                                     "name": "A neural network learns when it should not be trusted",
-        //                                     "doc_id": 15,
-        //                                     "value": 18
-        //                                 }
-        //                             ]
-        //                         },
-        //                         {
-        //                             "name": "Engineering", 
-        //                             "children": [
-        //                                 {
-        //                                     "name": "How the F@!# Did This Giant Whale Tail Save a Derailed Train?", 
-        //                                     "doc_id": 3, 
-        //                                     "value": 12
-        //                                 }
-        //                             ]
-        //                         }
-        //                     ]
-        //                 }
-        //             ]
-        //         },
-        //         {
-        //             "name": "Health", 
-        //             "children": [
-        //                 {
-        //                     "name": "Medicine",
-        //                     "children": [
-        
-        //                     ]
-        //                 }, 
-        //                 {
-        //                     "name": "Exercise",
-        //                     "children": [
-        
-        //                     ] 
-        //                 },
-        //                 {
-        //                     "name": "Wellness",
-        //                     "children": [
-        
-        //                     ]
-        //                 }
-        //             ]
-        //         },
-        //         {
-        //             "name": "Arts", 
-        //             "children": [
-        //                 {
-        //                     "name": "Visual Arts", 
-        //                     "children": [
-        //                         {
-        //                             "name": "Design", 
-        //                             "children": [
-        //                                 {
-        //                                     "name": "Editorial Design : New Brutalism Controversial Concrete", 
-        //                                     "doc_id": 4,
-        //                                     "value": 24
-        //                                 }
-        //                             ]
-        //                         },
-        //                         {
-        //                             "name": "Fine Arts", 
-        //                             "children": [
-        //                                 {
-        //                                     "name": "Prized Hockney Portrait Will Stay at London’s Royal Opera House Following Sale: Report", 
-        //                                     "doc_id": 5, 
-        //                                     "value": 14
-        //                                 }
-        
-        //                             ]
-        //                         },
-        //                         {
-        //                             "name": "Architecture", 
-        //                             "children": [
-        //                                 {
-        //                                     "name": "Public opinion has softened its views on Brutalism. That isn’t enough to stay the wrecking ball.",
-        //                                     "doc_id": 1,
-        //                                     "value": 35
-        //                                 }, 
-        //                                 {
-        //                                     "name": "Editorial Design : New Brutalism Controversial Concrete", 
-        //                                     "doc_id": 4,
-        //                                     "value": 24
-        //                                 }
-        //                             ]
-        //                         }
-        //                     ]
-        //                 }, 
-        //                 {
-        //                     "name": "Music & Performance Arts", 
-        //                     "children": [
-        //                         {
-        //                             "name": "Music",
-        //                             "children": [
-        
-        //                             ]
-        //                         }, 
-        //                         {
-        //                             "name": "Dance",
-        //                             "children": [
-        
-        //                             ]
-        //                         },
-        //                         {
-        //                             "name": "Theater",
-        //                             "children": [
-        
-        //                             ]
-        //                         }, 
-        //                         {
-        //                             "name": "Comedy",
-        //                             "children": [
-        
-        //                             ]
-        //                         }
-        //                     ]
-        //                 }
-        //             ]
-        //         },
-        //         {
-        //             "name": "Books", 
-        //             "children": [
-        
-        //             ]
-        //         },
-        //         {
-        //             "name": "Entertainment", 
-        //             "children": [
-        //                 {
-        //                     "name": "Film",
-        //                     "children": [
-        
-        //                     ]
-        //                 }, 
-        //                 {
-        //                     "name": "TV", 
-        //                     "children": [
-        
-        //                     ]
-        //                 }, 
-        //                 {
-        //                     "name": "Events",
-        //                     "children": [
-        
-        //                     ]
-        //                 }
-        //             ]
-        //         },
-        //         {
-        //             "name": "Food", 
-        //             "children": [
-        
-        //             ]
-        //         }, 
-        //         {
-        //             "name": "Politics", 
-        //             "children": [
-        
-        //             ]
-        //         },
-        //         {
-        //             "name": "Business", 
-        //             "children": [
-        
-        //             ]
-        //         },
-        //         {
-        //             "name": "Sports", 
-        //             "children": [
-        
-        //             ]
-        //         }, 
-        //         {
-        //             "name": "Other", 
-        //             "children": [
-        
-        //             ]
-        //         }
-        //     ]
-        // }
         let root = tagTree;
         
         // Constructs a root note from specified hiearchical data (data = obj representing the root node)
@@ -308,12 +66,36 @@ const D3Dash = () => {
             nodes = pack(root).descendants(), // array of descendant nodes w/ parent, children, value, depth, x, y, r props
             view;
         
+        var defs = g.append("defs");
+            
+        var images = defs.selectAll(null)
+            .data(nodes)
+            .enter().append("pattern")
+            .attr("id", function(d) {
+                console.log(d)
+                if (d.children) {
+                    return 0
+                } else{
+                    console.log(`image${d.data.doc_id}`)
+                    return `image${d.data.doc_id}`
+                }
+                })
+            .attr("width", 1)
+            .attr("height", 1)
+            .attr("preserveAspectRatio", "none")
+            .append("svg:image")
+            .attr("xlink:href", function(d) {return d.children ? '' : d.data.img})
+            .attr("width", function(d) {return d.r})
+            .attr("height", function(d) {return d.r})
+            .attr("preserveAspectRatio", "xMidYMid slice")
+            
+
         // Append group (circle)
         var circle = g.selectAll("circle")
             .data(nodes) // join with nodes array
             .enter().append("circle") // for each node add a circle
             .attr("class", function(d) { return d.parent ? d.children ? "node" : "node node--leaf" : "node node--root"; }) // assign class names to each circle
-            .style("fill", function(d) { return d.children ? color(d.depth) : null; }) // give circle a depth-based color
+            .style("fill", function(d) { return d.children ? color(d.depth) : `url(#image${d.data.doc_id})` }) // give circle a depth-based color
             .style("fill-opacity", function(d) { return d.value > 0 ? 1 : 0; })
             .on("dblclick", function(d) { if (focus !== d) zoom(d), d3.event.stopPropagation(); })
             
@@ -342,17 +124,19 @@ const D3Dash = () => {
 
         svg
             .style("background", color(-1))
-            .on("dblclick", function() { zoom(root); });
+            .on("dblclick", function() { zoom(root); }); // apply zoom behavior to root (the selection)
+            // bind event listeners for zooming to root
 
-        zoomTo([root.x, root.y, root.r * 2 + margin]);
+        zoomTo([root.x, root.y, root.r * 2 + margin]); // initiate position
 
-        function zoom(d) {
-            var focus0 = focus; focus = d;
+        function zoom(d) { // create a new zoom behavior
+            var focus0 = focus; focus = d; // 
 
             var transition = d3.transition()
-                .duration(d3.event.altKey ? 7500 : 750)
-                .tween("zoom", function(d) {
-                var i = d3.interpolateZoom(view, [focus.x, focus.y, focus.r * 2 + margin]);
+                .duration(d3.event.altKey ? 7500 : 750) // specify per-element duration (ms), slower if altKey is pressed
+                .tween("zoom", function(d) { // register custom tween for "zoom", when transition starts, the func runs
+                var i = d3.interpolateZoom(view, [focus.x, focus.y, focus.r * 2 + margin]); // interpolation method that
+                // applies between 2 views (start and end), each is [cx, cy, width] (center (x,y) and size of viewport )
                 return function(t) { zoomTo(i(t)); };
                 });
 
@@ -361,12 +145,19 @@ const D3Dash = () => {
                 .style("fill-opacity", function(d) { return d.parent === focus ? 1 : 0; })
                 .on("start", function(d) { if (d.parent === focus) this.style.display = "inline"; })
                 .on("end", function(d) { if (d.parent !== focus) this.style.display = "none"; });
+
+            transition.selectAll("pattern")
+                .style("fill-opacity", 0)
+                
         }
 
         function zoomTo(v) {
             var k = diameter / v[2]; view = v;
             node.attr("transform", function(d) { return "translate(" + (d.x - v[0]) * k + "," + (d.y - v[1]) * k + ")"; });
-            circle.attr("r", function(d) { return d.r * k; });
+            circle.attr("r", function(d) { return d.r * k; })
+            images
+            .attr("width", function(d) { return d.r * k * 2; })
+            .attr("height", function(d) { return d.r * k * 2 ; })
         }
 
     }
