3afaf2bcaaf820df237e8cf3917664aa65435b77
li-lauren <laurenlidev@gmail.com>
2020-12-20 22:02:08 +0000

    Clean up unneeded log statements

--- static/js/DocListing.jsx
+++ static/js/DocListing.jsx
@@ -1,10 +1,11 @@
+// Listing for an individual article
+
 const DocListing = ({doc}) => {
     const history = useHistory()
     const [isHovering, setIsHovering] = useState(false)
     const created_at = new Date(doc.created_at)
 
     const getDocDets = (doc_id, e) => {
-        console.log(`doc_id: ${doc_id}`)
         e.preventDefault()
         
         fetch(`/docs/${doc_id}`)
@@ -20,10 +21,15 @@ const DocListing = ({doc}) => {
             onMouseEnter={() => setIsHovering(true)}
             onMouseLeave={() => setIsHovering(false)}
         >
-            <a href="" key={doc.doc_id} onClick={(e)=> getDocDets(doc.doc_id, e)} >
+            <a 
+                href="" 
+                key={doc.doc_id} 
+                onClick={(e)=> getDocDets(doc.doc_id, e)} 
+            >
                 {doc.title}
             </a>
             <br/>
+            
             { isHovering ?
                 <a id="doc-timestamp">
                     {moment(created_at).format('MMMM Do, YYYY')}
